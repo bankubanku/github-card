f67793e054b42748d7f952293d4348caf474b610
bankubanku <wozniczka.jakub@proton.me>
2024-04-09 20:18:45 +0200

    appending image and name

--- search.js
+++ search.js
@@ -1,3 +1,41 @@
+function appendImg(mainSection, src, alt, id) {
+    const pfp = document.createElement("img");
+    pfp.src = src; 
+    pfp.alt = alt;
+    pfp.id = id;
+    mainSection.appendChild(pfp);
+}
+
+function appendInfo(mainSection, tag, content, id) {
+    const name = document.createElement(tag);
+    const textNode = document.createTextNode(content);
+    name.id = id
+    name.appendChild(textNode);
+    mainSection.appendChild(name);
+}
+
+
+function displayData(userData) {
+    const chosenData = [
+        "login", 
+        "repos_url", 
+        "company", 
+        "location", 
+        "bio", 
+        "email", 
+        "public_repos", 
+        "followers", 
+        "following", 
+        "created_at"
+    ];
+    
+    const mainSection = document.getElementsByTagName("main")[0];
+
+    appendImg(mainSection, userData.avatar_url, "profile picture", "pfp");
+    appendInfo(mainSection, "h1", userData.name, "name");
+}
+
+
 async function getData(event) {
     event.preventDefault(); // prevents from reloading the page
     
@@ -5,5 +43,6 @@ async function getData(event) {
     const response = await fetch(`https://api.github.com/users/${user}`);
     const userData = await response.json();
 
-    document.getElementById("data").innerHTML = JSON.stringify(userData);
+    // document.getElementById("data").innerHTML = JSON.stringify(userData);
+    displayData(userData);
 }
