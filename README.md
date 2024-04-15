fa7676668d769362b37a926888df5bf663ddcf29
bankubanku <wozniczka.jakub@proton.me>
2024-04-15 20:59:09 +0200

    Analyzed repos data and displayed it

---
+++ README.md
@@ -0,0 +1,2 @@
+# github card 
+It is a simple web app that displays some basic info about given user. Most of that information is easly found on original github user page, but I wanted to learn about fetch api and async/await. 
--- search.js
+++ search.js
@@ -1,3 +1,20 @@
+// function getUniqueArray(arr) {
+//     const outputArr = [];
+//     let count = 0;
+//     let start = false;
+//
+//     for (let j = 0; j < arr.length; j++) {
+//         for (let k = 0; k < arr.length; k++) {
+//             if (arr[j] == outputArr[k]) {
+//
+//             }
+//         }
+//         count++;
+//         if (count)
+//     }
+// }
+
+
 function appendImg(section, src, alt, id) {
     const pfp = document.createElement("img");
     pfp.src = src; 
@@ -30,17 +47,61 @@ function createMainInfoSection(userData, mainSection) {
 function createUserInfoSection(userData, mainSection) {
     const userInfo = document.createElement("div");
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var createdAtDate = new Date(userData.created_at); 
+    let createdAtDate = new Date(userData.created_at); 
     createdAtDate = `${months[createdAtDate.getMonth()]} ${createdAtDate.getFullYear()}`
 
     userInfo.id = "user-info";
     mainSection.appendChild(userInfo);
     
-    appendInfo(userInfo, "h1", `About ${userData.name}:`, "about-user")
+    appendInfo(userInfo, "h1", `About ${userData.name}:`, "about-user");
     appendInfo(userInfo, "h5", `Follows ${userData.following} people`, "following");
     appendInfo(userInfo, "h5", `Followed by ${userData.followers}`, "followers");
     appendInfo(userInfo, "h5", `Member since ${createdAtDate}`, "created-at");
     appendInfo(userInfo, "h5", `Lives in ${userData.location}`, "location");
+}
+
+
+function getStarsNumber(reposData) {
+    let starsCount = 0;
+    let forksCount = 0;
+    let usedLanguagesArr = [];
+    let usedLanguages = "";
+
+    for (let x in reposData) {
+        starsCount += reposData[x].stargazers_count;
+        forksCount += reposData[x].forks_count;
+        usedLanguagesArr.push(reposData[x].language);
+    }
+    
+    usedLanguagesArr = [...new Set(usedLanguagesArr)]; 
+    for (let x of usedLanguagesArr) {
+        usedLanguages +=  x + ", ";
+    }
+    usedLanguages = usedLanguages.slice(0, -2); 
+
+    return {
+        starsCount:starsCount,
+        forksCount:forksCount,
+        usedLanguages:usedLanguages
+    }
+}
+
+function createReposInfoSection(userData, mainSection) {
+    const reposInfo = document.createElement("div");
+    
+    reposInfo.id = "repos-info";
+    mainSection.appendChild(reposInfo);
+
+    getReposData(userData.repos_url)
+    .then(reposData => {
+            appendInfo(reposInfo, "h1", `About ${userData.name}'s repositories`, "about-repos");
+            appendInfo(reposInfo, "h5", `${userData.name} created/forked ${userData.public_repos} repos`, "repos-number");
+
+            const reposAnalyzedData = getStarsNumber(reposData);
+            appendInfo(reposInfo, "h5", `Starred ${reposAnalyzedData.starsCount} times`, "stars-count");
+            appendInfo(reposInfo, "h5", `Forked ${reposAnalyzedData.forksCount} times`, "forks-count");
+            appendInfo(reposInfo, "h5", `Repositories are build with ${reposAnalyzedData.usedLanguages}`, "languages");
+        })
 }
 
 
@@ -49,10 +110,17 @@ function displayData(userData) {
 
     createMainInfoSection(userData, mainSection);
     createUserInfoSection(userData, mainSection);
+    createReposInfoSection(userData, mainSection);
 }
 
 
-async function getData(event) {
+async function getReposData(reposURL) {
+    const response = await fetch(reposURL);
+    const reposData = await response.json();
+    return reposData
+}
+
+async function getUserData(event) {
     event.preventDefault(); // prevents from reloading the page
     
     const user = document.getElementById("search-input").value
