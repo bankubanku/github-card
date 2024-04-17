6ad3309d10f1e416a848128e48951fc983e8f5ef
bankubanku <wozniczka.jakub@proton.me>
2024-04-17 17:42:50 +0200

    added error handling

--- search.js
+++ search.js
@@ -1,20 +1,3 @@
-// function getUniqueArray(arr) {
-//     const outputArr = [];
-//     let count = 0;
-//     let start = false;
-//
-//     for (let j = 0; j < arr.length; j++) {
-//         for (let k = 0; k < arr.length; k++) {
-//             if (arr[j] == outputArr[k]) {
-//
-//             }
-//         }
-//         count++;
-//         if (count)
-//     }
-// }
-
-
 function appendImg(section, src, alt, id) {
     const pfp = document.createElement("img");
     pfp.src = src; 
@@ -38,9 +21,17 @@ function createMainInfoSection(userData, mainSection) {
     mainSection.appendChild(mainInfo);
 
     appendImg(mainInfo, userData.avatar_url, "profile picture", "pfp");
-    appendInfo(mainInfo, "h1", userData.name, "name");
-    appendInfo(mainInfo, "h2", userData.login, "login");
-    appendInfo(mainInfo, "P", userData.bio, "bio");
+    if (userData.name === null) {
+        userData.name = userData.login
+        appendInfo(mainInfo, "h1", userData.name, "name");
+    } else {
+        appendInfo(mainInfo, "h1", userData.name, "name");
+        appendInfo(mainInfo, "h2", userData.login, "login");
+    } 
+
+    if (userData.bio !== null) {
+        appendInfo(mainInfo, "P", userData.bio, "bio");
+    }
 }
 
 
@@ -54,10 +45,28 @@ function createUserInfoSection(userData, mainSection) {
     mainSection.appendChild(userInfo);
     
     appendInfo(userInfo, "h1", `About ${userData.name}:`, "about-user");
-    appendInfo(userInfo, "h5", `Follows ${userData.following} people`, "following");
-    appendInfo(userInfo, "h5", `Followed by ${userData.followers}`, "followers");
+
+    if (userData.following === 0) {
+        appendInfo(userInfo, "h5", `Follows nobody`, "following");
+    } else if (userData.following === 1) {
+        appendInfo(userInfo, "h5", `Follows only 1 person`, "following");
+    } else {
+        appendInfo(userInfo, "h5", `Follows ${userData.following} people`, "following");
+    } 
+
+    if (userData.followers === 0) {
+        appendInfo(userInfo, "h5", "Followed by nobody", "followers");
+    } else if (userData.followers === 1) {
+        appendInfo(userInfo, "h5", "Followed by only 1 person", "followers");
+    } else {
+        appendInfo(userInfo, "h5", `Followed by ${userData.followers}`, "followers");
+    } 
+
     appendInfo(userInfo, "h5", `Member since ${createdAtDate}`, "created-at");
-    appendInfo(userInfo, "h5", `Lives in ${userData.location}`, "location");
+
+    if (userData.location !== null) {
+        appendInfo(userInfo, "h5", `Lives in ${userData.location}`, "location");
+    }
 }
 
 
@@ -94,23 +103,55 @@ function createReposInfoSection(userData, mainSection) {
 
     getReposData(userData.repos_url)
     .then(reposData => {
-            appendInfo(reposInfo, "h1", `About ${userData.name}'s repositories`, "about-repos");
-            appendInfo(reposInfo, "h5", `${userData.name} created/forked ${userData.public_repos} repos`, "repos-number");
+            if (userData.public_repos == 0) {
+                appendInfo(reposInfo, "h1", `${userData.name} has no public repositories :c`, "repos-number");
+            } else {
+                appendInfo(reposInfo, "h1", `About ${userData.name}'s repositories`, "about-repos");
 
-            const reposAnalyzedData = getStarsNumber(reposData);
-            appendInfo(reposInfo, "h5", `Starred ${reposAnalyzedData.starsCount} times`, "stars-count");
-            appendInfo(reposInfo, "h5", `Forked ${reposAnalyzedData.forksCount} times`, "forks-count");
-            appendInfo(reposInfo, "h5", `Repositories are build with ${reposAnalyzedData.usedLanguages}`, "languages");
+               if (userData.public_repos === 1) {
+                    appendInfo(reposInfo, "h5", `${userData.name} created/forked only one repo`, "repos-number");
+                } else {
+                    appendInfo(reposInfo, "h5", `${userData.name} created/forked ${userData.public_repos} repos`, "repos-number");
+                } 
+
+                const reposAnalyzedData = getStarsNumber(reposData);
+                if (reposAnalyzedData.starsCount === 0) {
+                    appendInfo(reposInfo, "h5", `No stars :<`, "stars-count");
+                } else if (reposAnalyzedData.starsCount === 1) {
+                    appendInfo(reposInfo, "h5", `Starred only once`, "stars-count");
+                } else {
+                    appendInfo(reposInfo, "h5", `Starred ${reposAnalyzedData.starsCount} times`, "stars-count");
+                } 
+
+                if (reposAnalyzedData.forksCount === 0) {
+                    appendInfo(reposInfo, "h5", `Forked by nobody :<`, "forks-count");
+                } else if (reposAnalyzedData.forksCount === 1) {
+                    appendInfo(reposInfo, "h5", `Forked only once`, "forks-count");
+                } else {
+                    appendInfo(reposInfo, "h5", `Forked ${reposAnalyzedData.forksCount} times`, "forks-count");
+                } 
+
+                appendInfo(reposInfo, "h5", `Repositories are build with ${reposAnalyzedData.usedLanguages}`, "languages");
+            }
         })
 }
 
 
 function displayData(userData) {
     const mainSection = document.getElementsByTagName("main")[0];
+    
+    mainSection.innerHTML = '';
 
     createMainInfoSection(userData, mainSection);
     createUserInfoSection(userData, mainSection);
     createReposInfoSection(userData, mainSection);
+}
+
+
+function displayError(message) {
+    const mainSection = document.getElementsByTagName("main")[0];
+    
+    mainSection.innerHTML = `<h1>${message}</h1>`;
 }
 
 
@@ -123,10 +164,18 @@ async function getReposData(reposURL) {
 async function getUserData(event) {
     event.preventDefault(); // prevents from reloading the page
     
-    const user = document.getElementById("search-input").value
-    const response = await fetch(`https://api.github.com/users/${user}`);
-    const userData = await response.json();
+    const user = document.getElementById("search-input").value;
 
-    // document.getElementById("data").innerHTML = JSON.stringify(userData);
-    displayData(userData);
+    try {
+        const response = await fetch(`https://api.github.com/users/${user}`);
+        if (response.ok) {
+            const userData = await response.json();
+            displayData(userData);
+        } else {
+            displayError("Are you sure this user exists? :>");
+        }
+    } catch (error) {
+        console.error(error);
+        displayError("Sorry, the network error occured :<");
+    }
 }
